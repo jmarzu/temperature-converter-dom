46e42c7be4b40e3bc1c0e1935c9ffedf2373d621
Joe Marzullo <jmarzu13@gmail.com>
2016-05-27 17:04:28 -0700

    Joe Marzullo Temp Converter

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
-console.log('Hello, front end');
+console.log('dom content in progress');
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('dom loader');
+
+var suby = document.getElementById('subBtn');
+
+suby.addEventListener('click', function(event) {
+  event.preventDefault();
+  var fair = document.getElementById('numInput').value;
+  document.getElementById('outputTemp').innerHTML = fairChange(fair);
+
+});
+
+var subyC = document.getElementById('subBtnCel');
+
+subyC.addEventListener('click', function(event) {
+  event.preventDefault();
+  var cel = document.getElementById('numInput').value;
+  document.getElementById('outputTemp').innerHTML = celChange(cel);
+});
+
+var fairChange = function(fair) {
+  return ((fair * (9/5)) + 32);
+}
+
+var celChange = function(cel) {
+  return ((cel - 32) + (5/9));
+}
+
+});
